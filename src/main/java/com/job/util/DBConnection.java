abff94bd4ff36f869f67c54350832501da67b1e2
Suthan-T <suthanthadeus2006@gmail.com>
2026-06-30 17:22:02 +0530

    Companies servlet backend done

--- src/main/java/com/job/util/DBConnection.java
+++ src/main/java/com/job/util/DBConnection.java
@@ -8,7 +8,7 @@ public class DBConnection {
 
         private static final String url="jdbc:postgresql://localhost:5432/job_guidance_system";
         private static final String username="postgres";
-        private static final String password="enteryourpasswo";
+        private static final String password="enteryourpassword";
 
         public static Connection getConnection(){
             try{
