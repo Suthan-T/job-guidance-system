071c637208c2e29008b62fb4e87fa7afe35e69f3
Suthan-T <suthanthadeus2006@gmail.com>
2026-06-30 18:02:07 +0530

    Companies Servlet Backend done

--- src/main/java/com/job/dao/CompaniesDAO.java
+++ src/main/java/com/job/dao/CompaniesDAO.java
@@ -17,22 +17,13 @@ public class CompaniesDAO{
 
         String sql="INSERT INTO companies(name, description, website, headquarters) VALUES(?,?,?,?)";
 
-<<<<<<< HEAD
         try(Connection con = DBConnection.getConnection();
         	PreparedStatement ps=con.prepareStatement(sql)){
-=======
-        try(
-            Connection con = DBConnection.getConnection();
->>>>>>> branch 'main' of https://github.com/Suthan-T/job-guidance-system.git
-
-<<<<<<< HEAD
+     
             ps.setString(1,company.getName());
             ps.setString(2,company.getDescription());
             ps.setString(3,company.getWebsite());
             ps.setString(4,company.getHeadquarters());
-=======
-            PreparedStatement ps=con.prepareStatement(sql)){
->>>>>>> branch 'main' of https://github.com/Suthan-T/job-guidance-system.git
 
             int rows = ps.executeUpdate();
 
@@ -74,38 +65,16 @@ public class CompaniesDAO{
     }
 
     //To read companies by thier ID
-<<<<<<< HEAD
+
     public Companies getCompanyById(int id) {
-=======
-    public Companies getByID(int sid){
->>>>>>> branch 'main' of https://github.com/Suthan-T/job-guidance-system.git
-        String sql="SELECT * FROM companies WHERE id=?";
+       String sql="SELECT * FROM companies WHERE id=?";
 
         try(Connection con=DBConnection.getConnection();
             PreparedStatement ps=con.prepareStatement(sql)){
-<<<<<<< HEAD
                ps.setInt(1,id);
-=======
-               ps.setInt(1,sid);
->>>>>>> branch 'main' of https://github.com/Suthan-T/job-guidance-system.git
                try(ResultSet rs=ps.executeQuery()){
                     if(rs.next()){
-<<<<<<< HEAD
-                    	Companies company = new Companies();
-=======
->>>>>>> branch 'main' of https://github.com/Suthan-T/job-guidance-system.git
-
-<<<<<<< HEAD
-                    	company.setId(rs.getInt("id"));
-                    	company.setName(rs.getString("name"));
-                    	company.setDescription(rs.getString("description"));
-                    	company.setWebsite(rs.getString("website"));
-                    	company.setHeadquarters(rs.getString("headquarters"));
-
-                    	return company;
-=======
                         Companies company=new Companies();
-                    
                         company.setId(rs.getInt("id"));
                         company.setName(rs.getString("name"));
                         company.setDescription(rs.getString("description"));
@@ -116,35 +85,26 @@ public class CompaniesDAO{
                         //System.out.println("Record Found!");
                         //System.out.println("| "+id+" |"+name+" |"+desc+" |"+web+" |"+head+" |");
                     }
-                    else{
-                        //System.out.println("No records found!! for ID:"+sid);
->>>>>>> branch 'main' of https://github.com/Suthan-T/job-guidance-system.git
+                   
                     }
+               catch(SQLException e){
+                   e.printStackTrace();
                }
-            }
-            catch(SQLException e){
-                e.printStackTrace();
-            }
-<<<<<<< HEAD
+            } catch (SQLException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+           
         return null;
-=======
-            return null;
->>>>>>> branch 'main' of https://github.com/Suthan-T/job-guidance-system.git
+
     }
 
 
-<<<<<<< HEAD
+
     //To update companies description
     public boolean updateCompany(Companies company){
         String sql="UPDATE companies SET name=?, description=?,website=?,headquarters=? WHERE id=?";	        
         try(Connection con=DBConnection.getConnection();
-=======
-    //To update comapnies description
-    public void updateDes(Companies companies){
-        String sql="UPDATE companies SET description=? WHERE name=?";
-        try(
-            Connection con=DBConnection.getConnection();
->>>>>>> branch 'main' of https://github.com/Suthan-T/job-guidance-system.git
             PreparedStatement ps=con.prepareStatement(sql)){
 
             ps.setString(1, company.getName());
